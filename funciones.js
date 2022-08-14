1d8ba0492cc513309831d84df4d2cf4039c18ea4
Pablo Berdasco <pablo.berdasco@simplidata.com.ar>
2022-08-14 09:47:01 -0300

    agregados varios

--- funciones.js
+++ funciones.js
@@ -9,6 +9,7 @@ function Setup(){
     bInterna ++;
     a++;
     b++;
+
 }
 function Continuacion(){
     console.log("Variable a= ", a );
@@ -31,4 +32,29 @@ function BorrarTextoCuerpo(){
     /* espacioTexto.innerText = " "; */
     espacioTexto.textContent = "";
 }
-    
+
+function CargarPagina(pagina){
+    location. href = pagina;
+}
+
+
+let textoNumero = "";
+let operando1 = 0;
+let operando2 = 0;
+
+function TeclaNumero(numero){
+    let espacioTexto = document.getElementById("texto");
+    espacioTexto.innerText += numero;
+}
+
+function TeclaOperacion(op){
+    if (op === '+'){
+
+    }
+    else if(op === '-'){
+
+    }
+    else if(op === '='){
+
+    }
+}

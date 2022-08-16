2e436247f2af77c9d1ce20cc8752a3befea75b8c
Pablo Berdasco <pablo.berdasco@simplidata.com.ar>
2022-08-16 15:18:06 -0300

    avances simon - ejecuta turno maquina

--- scripts/simon.js
+++ scripts/simon.js
@@ -1,16 +1,72 @@
 const sounds = [new Audio("./audio/do.mp3"), new Audio("./audio/re.mp3"), new Audio("./audio/mi.mp3"), new Audio("./audio/fa.mp3")] ;
+const numbers = [];
+const tiempoNota = 1.5;
+let sequenceNumber = 1;
+let turno = "maquina";
 
-function Simon(numero){
-   
-    sounds[numero-1].play();
+function SimonSetup(cantidad){
+    for (i=0; i < cantidad; i++){
+        numbers.push(getRndInteger(1,4));
+    }  
+}
 
-    let botonPresionado = document.getElementById("simon"+numero);
+async function SimonList(){
+    for (i=0; i < sequenceNumber + 1; i++){
+        await sleep(1000);
+        SimonPlayOne(numbers[i]); 
+    }
+    sequenceNumber++;
+    turno = "humano";
+}
+
+function SimonPress(numero){
+    if (turno === "humano"){
+        SimonPlayOne(numero);       
+    }    
+}
+
+
+
+async function SimonPlayOne(numero){
+    //sounds[numero-1].play();
+    AudioPlay(sounds[numero-1],tiempoNota);
+
+    let botonPresionado = document.getElementById(`simon${numero}`);
+
     let colorOrigen = botonPresionado.style.backgroundColor;
     botonPresionado.style.backgroundColor = 'orange';
-    setTimeout(function(){
-        botonPresionado.style.backgroundColor = colorOrigen;
-    }, 1000);
+
+    await sleep(700);
+    botonPresionado.style.backgroundColor = colorOrigen;
 
     let espacioTexto = document.getElementById("texto");
-    espacioTexto.innerText += numero;
+        espacioTexto.innerText += numero;
 }
+
+
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function sleep(ms) {
+    return new Promise(
+      resolve => setTimeout(resolve, ms)
+    );
+  }
+
+
+// para poder acortar el tiempo que suena cada nota.
+function AudioPlay(audio, time){
+	audio.currentTime=1;
+	audio.play();
+       
+    // check cada 1/10 de segundo si se cumplio time
+	setInterval(function(){
+		if(audio.currentTime > time){
+			audio.pause();
+		}
+	},100);
+}
+    
